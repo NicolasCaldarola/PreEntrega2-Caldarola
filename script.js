6f4c1307cc258ef93fa501e534d587dbd9d50dbb
Pablo Nicolas Caldarola <nicocalda3@gmail.com>
2023-10-11 21:42:40 -0300

    2da pre entrega

---
+++ script.js
@@ -0,0 +1,34 @@
+let username = "";
+let password = "";
+
+function ConfirmacionDeContrasenia () {
+    password = prompt("Ingresa una contraseña:"); 
+    let confirmPassword = prompt("Confirma tu contraseña:");
+   
+    const ContraseniaActual = []
+    ContraseniaActual.push (password)
+    ContraseniaActual.push (confirmPassword)
+    return ContraseniaActual;
+}
+
+function register() {
+  // Pedir al usuario que ingrese el nombre de usuario
+  this.username = prompt("Ingresa un nombre de usuario:"); 
+
+  while (this.username == null || this.username == "" || this.username== " " )  { 
+  username=prompt("Ingresa un nombre de usuario:")
+  }
+    let  contrasenia=ConfirmacionDeContrasenia ()
+   
+    // Verificar si la contraseña coincide con la confirmación
+    while (contrasenia[0] != contrasenia[1]) {
+    alert ("Las contrasenias no coinciden. Ingrese una nueva"); 
+    contrasenia=ConfirmacionDeContrasenia ();
+  }   
+}
+
+alert    ("SEGUNDA PRE ENTREGA");
+alert    ("Vamos a Registrarnos");
+register ();
+alert ("Usuario Registrado: " + this.username );
+alert    ("FIN")
